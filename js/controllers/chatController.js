e2f04c4b68b4e7ab12c22c77e0ddee98cbe8bde3
John Currie <johntcurrie@gmail.com>
2017-08-04 15:41:45 -0700

    Added ability to add user message and username

--- js/controllers/chatController.js
+++ js/controllers/chatController.js
@@ -1,7 +1,28 @@
-tinyChatApp.controller('chatController', ['$http', function($http) {
+tinyChatApp.controller('chatController', ['$http', '$scope', function($http, $scope) {
+
 	var vm = this;
 	vm.messages = [];
+	vm.lastId = 0;
 	$http.get('../../fixtures/fakedata.json').then(function(response) {
 		vm.messages = response.data.messages;
-	})
-}])
+		vm.lastId = vm.messages[vm.messages.length-1].id;
+	});
+
+	vm.username= '';
+	vm.message = '';
+
+	vm.addMessage = function() {
+		vm.date = new Date();
+		vm.epochSeconds = Math.round(vm.date.getTime() / 1000);
+		vm.temp = vm.messages;
+		vm.messageObject = {
+			id: vm.lastId++,
+			author: vm.username,
+			timestamp: vm.epochSeconds,
+			content: vm.message
+		}
+		vm.messages.push(vm.messageObject);
+		console.log(vm.messages)
+	}
+
+}]);
